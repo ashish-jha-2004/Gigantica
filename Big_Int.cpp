c01d114d4f4a939c2cd9a03953d9ddf618d2df27
Ashish Kumar <152195795+ashish-jha-2004@users.noreply.github.com>
2024-09-26 19:34:31 +0530

    Add files via upload

---
+++ Big_Int.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include"Big_Int.hpp"
+using namespace std;
+
+int main() {
+    bigint num1, num2;
+    cin >> num1 >> num2;
+    cout << num1 + num2 << '\n';
+    cout << num1 * num2 << '\n';
+    return 0;
+}
